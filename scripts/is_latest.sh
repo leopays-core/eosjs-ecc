d39f4d83ca8e62ebc4d8d0e6405ac64808195c83
Aleksandr Ivannikov <a.n.ivannikov@yandex.ru>
2020-09-22 00:00:54 +0700

    scripts

--- scripts/is_latest.sh
+++ scripts/is_latest.sh
@@ -8,12 +8,12 @@ IFS='\n' read -ra arry <<< "$tags"
 latest_tag="${arry[0]}"
 
 if [ "$latest_tag" == "" ]; then 
-    latest_tag="v0.0.0";
+  latest_tag="v0.0.0";
 else
-    tag_commit="$(git rev-list -n 1 ${latest_tag})";
-    if [ "$tag_commit" == "$current_commit" ]; then
-        is_latest=true;
-    fi
+  tag_commit="$(git rev-list -n 1 ${latest_tag})";
+  if [ "$tag_commit" == "$current_commit" ]; then
+    is_latest=true;
+  fi
 fi
 
 echo "tag_commit: ${tag_commit}";
